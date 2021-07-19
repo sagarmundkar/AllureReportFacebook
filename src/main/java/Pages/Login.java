0d788cbfef6058369f839303f303b32951f806e4
sagarmundkar <48950343+sagarmundkar@users.noreply.github.com>
2021-07-19 19:22:24 +0530

    Update Login Page

--- src/main/java/Pages/Login.java
+++ src/main/java/Pages/Login.java
@@ -25,8 +25,8 @@ public class Login extends BaseProgram {
     }
 
     public void LoginApplication() throws InterruptedException {
-        email.sendKeys("shreemundkar@gmail.com");
-        password.sendKeys("SagarMundkar@1234");
+        email.sendKeys("********");
+        password.sendKeys("*****");
         loginbutton.click();
         Thread.sleep(100);
 
